a83f51fac4556221ae89949aa49b134baa5206a3
elieljohn <ejmalabar88@gmail.com>
2024-04-21 18:03:57 +0100

    navbar-primary.component has multiple classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'saa-app',
-    templateUrl: './app.component.html',
-    encapsulation: ViewEncapsulation.None
+    templateUrl: './app.component.html'
 })
 
 export class AppComponent {
