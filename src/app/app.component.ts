b88f5f425ef2e2582e14d65d4afa7756590c7055
elieljohn <ejmalabar88@gmail.com>
2024-04-21 17:26:35 +0100

    Scoped styles and global styles combined

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
     selector: 'saa-app',
     templateUrl: './app.component.html',
-    encapsulation: ViewEncapsulation.None,
-    styleUrls: ['./styles/styles.scss']
+    // encapsulation: ViewEncapsulation.None,
+    // styleUrls: [
+    //     './shared/scss/root/normalize.scss',
+    //     './shared/scss/root/root.scss',
+    //     './shared/scss/root/links.scss',
+    //     './shared/scss/layout/app-footer.scss',
+    //     './shared/scss/layout/app-header.scss',
+    //     './shared/scss/layout/layout.scss',
+    //     './shared/scss/typography/headers.scss',
+    //     './shared/scss/typography/paragraphs.scss'
+    // ]
 })
 
 export class AppComponent {
